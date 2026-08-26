0589f124c2289051ba44fd0e66292231e36b6115
s025101 <junichirou.kita@gmail.com>
2026-08-26 21:52:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, jsonify, request
 import requests
 from datetime import datetime
@@ -103,4 +104,5 @@ def fetch_live_flight():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
